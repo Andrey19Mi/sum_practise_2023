d28c4778840fafdb62a208e368bffbaa77f1f9fc
Shalamaev_Kirill <112622898+HelpMySoul@users.noreply.github.com>
2023-07-03 19:17:45 +0300

    delete wrapper for config types and use base Config type as a wrapper instead (#7)
    * wrapper deleted
    
    * Fix derrived type mismatch in pre-serialization config building.
    
    ---------
    
    Co-authored-by: HelpMySoul <shalamaev.es.hms@gmail.com>
    Co-authored-by: Eugene Frolov <spanditime@gmail.com>

--- sum_practise_2023/sum_practise_2023/Document.cs
+++ sum_practise_2023/sum_practise_2023/Document.cs
@@ -176,18 +176,23 @@ namespace sum_practise_2023
             List<Config> cfg = new List<Config>();
             foreach(var ctl in Components)
             {
-                TextFieldConfig c = new TextFieldConfig();
                 try
                 {
+                    Config c;
                     if (ctl.comp is Label)
                     {
+                        c = new TextFieldConfig();
                         c.Deconstruct(ctl.comp);
+                    }// to ensure that we can easily add configs of other types, 
+                    else
+                    {
+                        throw new Exception();
                     }
+                    cfg.Add(c);
                 }catch
                 {
                     throw new Exception("Сomponent deconstruction error");
                 }
-                cfg.Add(c);
             }
             try
             {
@@ -228,7 +233,7 @@ namespace sum_practise_2023
                 {
                     try
                     {
-                        Components.Add(new Component(new ComponentWrapper(config as TextFieldConfig).Comp, this));
+                        Components.Add(new Component(config.Construct(), this));
                     } catch
                     {
                         throw new Exception("New component add error");

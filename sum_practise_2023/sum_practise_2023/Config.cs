a328c5ac91a649881ef13ac8197c04f65e35016f
Eugene Frolov <46240864+spanditime@users.noreply.github.com>
2023-07-03 21:45:02 +0300

    Add custom converter for Config derrived types (#8)

--- sum_practise_2023/sum_practise_2023/Config.cs
+++ sum_practise_2023/sum_practise_2023/Config.cs
@@ -17,7 +17,6 @@ namespace sum_practise_2023
     // if you made a new type of config add it below
     //[JsonDerivedType(typeof(Config), typeDiscriminator: "base")]
     // you have to add for each new config a new derived type and descriminator here
-    [JsonDerivedType(typeof(TextFieldConfig), typeDiscriminator: "TextField")]
 
     // !!!! Do not derive classes from each other even if they are similar, all classes should be derrived only from Config
     // and there must be methods Construct aand Deconstruct redefined
@@ -26,39 +25,48 @@ namespace sum_practise_2023
     // eg if you use MyGenericConfig type with parameters string and int you should specify:
     // [JsonDerivedType(typeof(MyGenericConfig<int>), typeDiscriminator: "GenericConfigInt")]
     // [JsonDerivedType(typeof(MyGenericConfig<string>), typeDiscriminator: "GenericConfigString")]
-    internal abstract class Config
+    internal interface Config : JsonSL.ITypeDiscriminator
     {
         //Deconstruct takes a non serializable control, checks its type and stores all of the data to be serialized in fields of its class
-        public abstract void Deconstruct(System.Windows.Forms.Control ctl);
+        void Deconstruct(System.Windows.Forms.Control ctl);
         // Constructs returns a control of a known type with all serializable data set to a control
-        public abstract System.Windows.Forms.Control Construct();
+        System.Windows.Forms.Control Construct();
     }
     internal class TextFieldConfig : Config
     {
+        public string TypeDiscriminator => nameof(TextFieldConfig);
+        public string FamilyName { get; set; }
+        public float Size { get; set; }
+        public FontStyle Style { get; set; }
         public string Text { get; set; }
-        public Point Location { get; set; }
-        public Font Font { get; set; }
-        public override void Deconstruct(System.Windows.Forms.Control ctl)
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public void Deconstruct(System.Windows.Forms.Control ctl)
         {
             if (ctl is System.Windows.Forms.Label && ctl != null) {
                 this.Text = ctl.Text;
-                this.Location = ctl.Location;
-                this.Font = ctl.Font;
+                this.X = ctl.Location.X;
+                this.Y = ctl.Location.Y;
+                this.FamilyName = ctl.Font.FontFamily.Name;
+                this.Size  = ctl.Font.Size;
+                this.Style = ctl.Font.Style;
             }
             else
             {
                 throw new ConfigException();
             }
         }
-        public override Control Construct()
+        public Control Construct()
         {
-            if (this.Text == null || this.Font == null || this.Font == null) {
+            if ( this.Text == null || this.FamilyName == null ) {
                 throw new ConfigException();
             }
             Control lb = new Label();
             lb.Text = Text;
-            lb.Location = Location;
-            lb.Font = Font;
+            lb.Location = new Point(X,Y);
+            // this one might throw an
+            lb.Font = new Font(FamilyName, Size, Style);
             return lb;
         }
     }
